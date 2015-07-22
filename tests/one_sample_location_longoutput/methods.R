744c0d6bdeeacdc08258370c2978210d5579d207
stephens999 <stephens999@gmail.com>
2015-07-21 20:49:24 -0500

    removed test that was based on old version of dsc

--- tests/one_sample_location_longoutput/methods.R
+++
@@ -1,7 +0,0 @@
-sourceDir("methods")
-methods=list()
-  methods[[1]] = list(name="mean",fn =mean.wrapper,args=NULL)
-  methods[[2]] = list(name="median",fn=median.wrapper,args=NULL)
-  methods[[3]] = list(name="winsor",fn=winsor.wrapper,args=NULL)
-
-
